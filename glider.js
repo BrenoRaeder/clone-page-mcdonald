1d763e997eeda59bd717b9c0f779f620f9e65158
Breno <brenoraeder97@gmail.com>
2021-08-02 12:33:09 -0300

    feat: adcionando media query

--- glider.js
+++ glider.js
@@ -1,5 +1,20 @@
-window.addEventListener('load', function(){
+window.addEventListener('pageshow', function(){
+if(window.matchMedia("(min-width: 770px)").matches)
+    {
     new Glider(document.querySelector('.glider'), {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      draggable: true,
+      dots: '.dots',
+      arrows: {
+        prev: '.glider-prev',
+        next: '.glider-next'
+      }
+    });
+  }
+else
+    {
+      new Glider(document.querySelector('.glider'), {
         slidesToShow: 1,
         slidesToScroll: 1,
         draggable: true,
@@ -9,6 +24,38 @@ window.addEventListener('load', function(){
           next: '.glider-next'
         }
       });
+    }
+})
+
+window.addEventListener('load', function(){
+  if(window.matchMedia("(min-width: 770px)").matches)
+      {
+      new Glider(document.querySelector('.glider'), {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        draggable: true,
+        dots: '.dots',
+        arrows: {
+          prev: '.glider-prev',
+          next: '.glider-next'
+        }
+      });
+    }
+  else
+      {
+        new Glider(document.querySelector('.glider'), {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          draggable: true,
+          dots: '.dots',
+          arrows: {
+            prev: '.glider-prev',
+            next: '.glider-next'
+          }
+        });
+      }
   })
+
+  
 
 
